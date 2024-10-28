a7b719614c6413fbd57e13861f4c2cbaf44e4ef9
Adilet <adiletkanybekovic03@gmail.com>
2024-10-28 20:42:47 +0100

    refactor: delete php

--- controllers/contacts.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$name = 'Contacts';
-require(__DIR__ . '/../views/contacts.view.php');
--- controllers/projects.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$name = 'Projects';
-require(__DIR__ . '/../views/projects.view.php');
